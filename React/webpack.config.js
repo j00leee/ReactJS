e32a3c6c70ed8e17289f5dece8aac1227a676b19
Julie <julie.minjung.chung@gmail.com>
2018-04-11 09:19:00 -0400

    new react tutorial in the process

---
+++ React/webpack.config.js
@@ -0,0 +1,30 @@
+/*
+setting webpack entry point to be main.js
+output path will be the place where the app will show
+development server: port 8080 (but we can choose any ports)
+setting babel loaders to look for js files 
+*/
+var config = {
+   entry: './main.js',
+   output: {
+      path:'/',
+      filename: 'index.js',
+   },
+   devServer: {
+      inline: true,
+      port: 8080
+   },
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+}
+module.exports = config;
